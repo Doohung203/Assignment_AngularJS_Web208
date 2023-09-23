c564b6cf974fff45cf0c5c639a184080415e3382
Doo03 <Doohung2003@gmail.com>
2023-09-23 15:42:09 +0700

    commit

--- Assignment_Web208/src/app/app.module.ts
+++ Assignment_Web208/src/app/app.module.ts
@@ -6,13 +6,20 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './page/homepage/homepage.component';
 import { ProductPageComponent } from './page/product-page/product-page.component';
 import { ProductDetailComponent } from './page/product-detail/product-detail.component';
-
+import { ProductComponent } from './page/admin/product/product.component';
+import { AddProductComponent } from './page/admin/add-product/add-product.component';
+import { EditProductComponent } from './page/admin/edit-product/edit-product.component';
+import { DeleteProductComponent } from './page/admin/delete-product/delete-product.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     ProductPageComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductComponent,
+    AddProductComponent,
+    EditProductComponent,
+    DeleteProductComponent,
   ],
   imports: [
     BrowserModule,
